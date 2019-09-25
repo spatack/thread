1f9922a78c03c6ec751df6e94bb013fd78059809
spatack <jasonlin58295@gamil.com>
2019-09-25 17:19:31 +0800

    增加start和run对比

--- src/main/java/InnerClassImp/Demo2.java
+++ src/main/java/InnerClassImp/Demo2.java
@@ -3,27 +3,33 @@ package InnerClassImp;
 /**
  * start 和 run 区别
  * 1.start：告诉jvm启动一个线程，并在线程内调用run方法
- * 2.run：未使用start情况下，当做普通的方法按顺序执行
+ * 2.run：未使用start情况下，当做普通的方法在主线程main里执行
  */
 public class Demo2 {
 
     public static void main(String[] args) {
         //创建2个Thread
-        Thread t1= new Thread(new Runnable() {
+        Thread t1 = new Thread(new Runnable() {
             public void run() {
-                for (int i = 0; i <5 ; i++) {
-                    System.out.println("线程t1 i:"+i);
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("当前线程" +
+                            Thread.currentThread().getName() + " " +
+                            Thread.currentThread().getId() +
+                            " i:" + i);
                 }
             }
-        });
+        }, "t1");
 
-        Thread t2 =new Thread(new Runnable() {
+        Thread t2 = new Thread(new Runnable() {
             public void run() {
-                for (int i = 0; i <5 ; i++) {
-                    System.out.println("线程t2 i:"+i);
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("当前线程" +
+                            Thread.currentThread().getName() + " " +
+                            Thread.currentThread().getId() +
+                            " i:" + i);
                 }
             }
-        });
+        }, "t2");
 
         // 以start方式调用run
         t1.start();
